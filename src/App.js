b333aba123dcd9f3b086503c88f5d9d7ac88fc8a
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-03-07 23:54:36 -0800

    Broke the code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import { Fragment } from "react";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from './components/UserProfile'
+
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const auth = useSelector((state) => state.counter.counter);
+
+
+  
   return (
     <Fragment>
+      {
+        auth &&  <UserProfile />
+      }
       <Header />
       <Auth />
       <Counter />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 import classes from './Header.module.css';
+import { useDispatch } from "react-redux";
+import { authActions } from '../store/index.js';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () =>{
+    dispatch(authActions.logout());
+  }
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
@@ -13,7 +20,7 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={handleLogout}>Logout</button>
           </li>
         </ul>
       </nav>
